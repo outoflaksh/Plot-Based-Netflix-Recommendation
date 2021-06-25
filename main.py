9bea3c609436f04c9f095d6f3a86a39f4cc68ad8
laksh <lakshyam38@gmail.com>
2021-06-25 19:19:47 +0530

    made the recommendation function

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def get_similarity_matrix():
+    dataset = pd.read_csv('./dataset/processed.csv')
+    count = TfidfVectorizer()
+    count_matrix = count.fit_transform(dataset['bag_of_words'])
+    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+
+    return cosine_sim
+
+def recommendations(title, cosine_sim = get_similarity_matrix()):
+    indices = pd.read_csv('./dataset/indices.csv')
+    recommended_movies = []
+    # gettin the index of the movie that matches the title
+    idx = indices[indices == title].index[0]
+
+    # creating a Series with the similarity scores in descending order
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+
+    # getting the indexes of the 10 most similar movies
+    top_10_indexes = list(score_series.iloc[1:11].index)
+    
+    # populating the list with the titles of the best 10 matching movies
+    for i in top_10_indexes:
+        recommended_movies.append((i, indices.loc[i]['title']))
+        
+    return recommended_movies
+
+
+dataset = pd.read_csv('./dataset/netflix_titles.csv')
+target = input("Enter movies/tv-show you want similar stuff for!: ")
+print("\n")
+recom = recommendations(target)
+for index, title in recom:
+    print(title + " (" + dataset.loc[index]['listed_in'] + ")")
+    print(dataset.loc[index]['description'])
+    print('\n')

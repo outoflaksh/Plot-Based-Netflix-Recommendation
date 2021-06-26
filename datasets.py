35b483dee5a099c97abdfbf47962696b0d3f864c
laksh <lakshyam38@gmail.com>
2021-06-26 15:46:29 +0530

    added separate file for dataset imports

---
+++ datasets.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+DATASET_FULL = pd.read_csv('./dataset/netflix_titles.csv')
+INDICES = pd.read_csv('./dataset/indices.csv')
+DATASET_PROCESSED = pd.read_csv('./dataset/processed.csv')

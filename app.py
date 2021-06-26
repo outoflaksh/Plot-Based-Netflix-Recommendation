846e95fcb55e9c5f771c1a1012f15a4993a43233
laksh <lakshyam38@gmail.com>
2021-06-26 15:47:38 +0530

    added dataset imports

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from main import recommendations
-import pandas as pd
-
-
-dataset = pd.read_csv('./dataset/netflix_titles.csv')
+from datasets import DATASET_FULL
 
 while True:
     target = input("Enter movies/tv-show you want similar stuff for!: ")
     print("\n")
     recom = recommendations(target)
+
     if type(recom) == str:
         print(recom)
     else:
         for index, title in recom:
-            print(title + " (" + dataset.loc[index]['type'] + ")")
-            print(dataset.loc[index]['description'])
+            print(title + " (" + DATASET_FULL.loc[index]['type'] + ")")
+            print(DATASET_FULL.loc[index]['description'])
             print('\n')
 
     ch = input("\nMore?: ")

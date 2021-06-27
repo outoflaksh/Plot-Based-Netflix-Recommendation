6aa53a8e8678b2542a0ab2122067cdfabed5a378
laksh <lakshyam38@gmail.com>
2021-06-27 12:42:15 +0530

    made Flask API endpoint

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
+from flask import Flask, request, jsonify
 from main import recommendations
 from datasets import DATASET_FULL
 
-while True:
-    target = input("Enter movies/tv-show you want similar stuff for!: ")
-    print("\n")
-    recom = recommendations(target)
+app = Flask(__name__)
 
-    if type(recom) == str:
-        print(recom)
-    else:
-        for index, title in recom:
-            print(title + " (" + DATASET_FULL.loc[index]['type'] + ")")
-            print(DATASET_FULL.loc[index]['description'])
-            print('\n')
+@app.route("/recommend", methods = ["POST"])
+def recommend():
+    title = request.get_json()['title']
+    recom = recommendations(title)
+    
+    return jsonify({'message' : recom})
+    
 
-    ch = input("\nMore?: ")
-    if ch == 'n':
-        break
+if __name__ == "__main__":
+    app.run()

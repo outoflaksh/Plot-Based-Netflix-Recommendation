fe39a0fc799a68f54f268a4d31160efc680374ac
laksh <lakshyam38@gmail.com>
2022-01-13 17:32:27 +0530

    shifted from flask to fastapi

--- api.py
+++ api.py
@@ -1,28 +1,25 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from fastapi import FastAPI, HTTPException
 from main import recommendations
 from datasets import DATASET_FULL
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
 
-@app.route("/recommend", methods=["GET"])
-@cross_origin()
-def get_recommendation():
-    title: str = request.args.get("title")
-    recom = recommendations(title.title())
+
+@app.get("/")
+async def read_index():
+    return {"message": "Welcome!"}
+
+
+@app.get("/recommend")
+async def get_recommendation(title: str):
+    recom = recommendations(title)
     if not recom:
-        return jsonify({"status": 404, "data": recom }), 404
+        raise HTTPException(status_code=404, detail="Requested title not found!")
 
-    return jsonify({'data': recom, "status": 200})
-
-
-@app.route("/titles", methods=["GET"])
-@cross_origin()
-def read_all_movies():
-    movies = list(DATASET_FULL['title'])
-    return jsonify({"status": 200, "data": movies}), 200
+    return {"data": recom, "status": 200}
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.get("/titles")
+async def read_all_movies():
+    movies = list(DATASET_FULL["title"])
+    return {"status": 200, "data": movies}, 200

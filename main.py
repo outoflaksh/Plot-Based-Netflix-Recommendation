c1c0f02cbfcbe30f974acdab0cad4b16dcfaa375
laksh <lakshyam38@gmail.com>
2022-01-13 17:06:07 +0530

    changes to accomodate varying textcases

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import pickle
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -6,20 +8,31 @@ from datasets import DATASET_FULL, DATASET_PROCESSED, INDICES
 
 
 def get_similarity_matrix(dataset=DATASET_PROCESSED):
-    count = TfidfVectorizer()
-    count_matrix = count.fit_transform(dataset['bag_of_words'])
-    cosine_sim = cosine_similarity(count_matrix, count_matrix)
+    try:
+        with open("similarity_matrix.pickle", "rb") as f:
+            similarity_matrix = pickle.load(f)
+            return similarity_matrix
+    except:
+        count = TfidfVectorizer()
+        count_matrix = count.fit_transform(dataset['bag_of_words'])
+        cosine_sim = cosine_similarity(count_matrix, count_matrix)
 
-    return cosine_sim
+        with open("similarity_matrix.pickle", "wb") as f:
+            pickle.dump(cosine_sim, f)
+        
+        return cosine_sim
 
 
-def recommendations(title, cosine_sim=get_similarity_matrix(), indices=INDICES, df=DATASET_FULL):
-    if title not in indices.values:
+def recommendations(title: str, cosine_sim=get_similarity_matrix(), indices=INDICES, df=DATASET_FULL):
+    title = title.lower()
+    title_list = np.array([x[1].lower() for x in indices.values])
+
+    if title not in title_list:
         return []
 
     recommended_movies = []
     # gettin the index of the movie that matches the title
-    idx = indices[indices['title'] == title].index[0]
+    idx = indices[indices['title'].str.lower() == title].index[0]
 
     # creating a Series with the similarity scores in descending order
     score_series = pd.Series(cosine_sim[idx]).sort_values(ascending=False)
